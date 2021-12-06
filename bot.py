20993c9a1c7606e77e63d34e7d891a809809fe20
Danil Kazakov <danil.kazakov.1980@mail.ru>
2021-12-06 17:30:23 +0700

    Bot Start

--- bot.py
+++ bot.py
@@ -1,16 +1,45 @@
-import config
 import logging
 
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from sqlighter import SQLighter
 
+# задаем уровень логов
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=config.API_TOKEN)
+# инициализируем бота
+bot = Bot(token='5015591679:AAEn0naed6fNPHv2WVlAgXQQDUIUfzzj2I8')
 dp = Dispatcher(bot)
+# инициализируем соединение с БД
+db = SQLighter('db.db')
+
+# Команда активации подписки
+@dp.message_handler(commands=['subscribe'])
+async def subscribe(message: types.Message):
+    if (not db.subscriber_exists(message.from_user.id)):
+        # если юзера нет в базе, добавляем его
+        db.add_subscriber(message.from_user.id)
+    else:
+        # если он уже есть, то просто обновляем ему статус подписки
+        db.update_subscription(message.from_user.id, True)
+
+    await message.answer(
+        "Вы успешно подписались на рассылку!\nЖдите, скоро выйдут новые обзоры и вы узнаете о них первыми =)")
 
 
-async def echo(message: types.Message):
-    await message.answer(message.text)
+# Команда отписки
+@dp.message_handler(commands=['unsubscribe'])
+async def unsubscribe(message: types.Message):
+    if (not db.subscriber_exists(message.from_user.id)):
+        # если юзера нет в базе, добавляем его с неактивной подпиской (запоминаем)
+        db.add_subscriber(message.from_user.id, False)
+        await message.answer("Вы итак не подписаны.")
+    else:
+        # если он уже есть, то просто обновляем ему статус подписки
+        db.update_subscription(message.from_user.id, False)
+        await message.answer("Вы успешно отписаны от рассылки.")
 
-if __name__  == '__main__':
+# запускаем лонг поллинг
+if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
